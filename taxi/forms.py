991548849bdd8ce20a9b7a68f4d5956d3883c4b8
lassisterv16 <lassisterv16@gmail.com>
2026-06-01 21:50:24 +0200

    solution

---
+++ taxi/forms.py
@@ -0,0 +1,48 @@
+from django import forms
+from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import UserCreationForm
+from django.core.validators import RegexValidator
+
+from taxi.models import Driver, Car
+
+license_structure_validator = RegexValidator(
+    regex=r"^[A-Z]{3}\d{5}$",
+    message="""License number must consist only of 8 characters,
+    first 3 characters are uppercase letters,
+    last 5 characters are digits.
+    """
+)
+
+
+class DriverCreationForm(UserCreationForm):
+    license_number = forms.CharField(
+        required=True,
+        validators=[license_structure_validator]
+    )
+
+    class Meta(UserCreationForm.Meta):
+        model = Driver
+        fields = UserCreationForm.Meta.fields + ("first_name", "last_name", )
+
+
+class DriverLicenseUpdateForm(forms.ModelForm):
+    license_number = forms.CharField(
+        required=True,
+        validators=[license_structure_validator]
+    )
+
+    class Meta:
+        model = Driver
+        fields = ["license_number", ]
+
+
+class CarForm(forms.ModelForm):
+    drivers = forms.ModelMultipleChoiceField(
+        queryset=get_user_model().objects.all(),
+        widget=forms.CheckboxSelectMultiple,
+        required=True
+    )
+
+    class Meta:
+        model = Car
+        fields = "__all__"
